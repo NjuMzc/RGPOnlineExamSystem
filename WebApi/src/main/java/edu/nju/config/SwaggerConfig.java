2183d608da0a2dbcfe8991847c33dd23459e39ee
Mzc <mzc19960830@163.com>
2017-10-24 15:58:39 +0800

    小的修改

--- WebApi/src/main/java/edu/nju/config/SwaggerConfig.java
+++ WebApi/src/main/java/edu/nju/config/SwaggerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -46,8 +47,9 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo(){
         return new ApiInfoBuilder()
-                .title("WebApi")
+                .title("RGP在线考试系统 Web Api文档")
                 .version("1.0")
+                .contact(new Contact("马张驰","","141250091@smail.nju,edu,cn"))
                 .build();
     }
 
